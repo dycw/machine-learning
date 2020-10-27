ad04f2c72f000e6b4b51d01b7cf943fef97f547e
Derek Wan <d.wan@icloud.com>
2020-10-27 10:55:25 +0800

    Add two animation ipynbs, back to papermill for testing (#10)
    * Add animation example to notebook
    
    * Add the Animation class
    
    * Make hidden
    
    * Add two animation notebooks, some random MPL gifs
    
    * gitpython is core requirement
    
    * Simplify some notebooks
    
    * Re-run
    
    * Re-run
    
    * Re-run
    
    * Re-do animation bokeh
    
    * Re-run
    
    * Re-do animationmpl
    
    * Fix seed
    
    * Fix seed 3
    
    * Re-enable animations
    
    * Ignore data/
    
    * Revert notebook
    
    * Simpler
    
    * Run bokeh download
    
    * Black
    
    * Slim animation mpl
    
    * Back to papermill testing
    
    * Add celluloid for now
    
    * Animation
    
    * Try conda for environment in workflows
    
    * Add conda-forge
    
    * Fix pip requirements
    
    * Clean notebooks
    
    * Fix repo root missing in notebook
    
    * Simpler pre-commit github action
    
    * Revert "Simpler pre-commit github action"
    
    This reverts commit c447122aac78f62bf2f29e6ad94968645b32c06b.
    
    * Simpler pre-commit action
    
    * Use seismic colormap

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,14 +4,12 @@ from pathlib import Path
 from typing import Iterator
 
 from git import Repo
+from papermill import execute_notebook
 from pytest import mark
-from pytest_notebook.nb_regression import NBRegressionFixture
 
 
 def _yield_notebook_paths() -> Iterator[Path]:
-    root = Path(
-        Repo(".", search_parent_directories=True).working_tree_dir,
-    ).joinpath("machine-learning")
+    root = Path(Repo(".", search_parent_directories=True).working_tree_dir)
     for path in root.glob("**/*.ipynb"):
         if not any(p == ".ipynb_checkpoints" for p in path.parts):
             yield root.joinpath(path)
@@ -22,8 +20,8 @@ def _yield_notebook_paths() -> Iterator[Path]:
     list(_yield_notebook_paths()),
     ids=str,
 )
-def test_notebooks(
-    nb_regression: NBRegressionFixture,
-    notebook_path: Path,
-) -> None:
-    nb_regression.check(str(notebook_path), raise_errors=True)
+def test_notebooks(notebook_path: Path, tmp_path: Path) -> None:
+    execute_notebook(
+        str(notebook_path),
+        str(tmp_path.joinpath(notebook_path.name)),
+    )
